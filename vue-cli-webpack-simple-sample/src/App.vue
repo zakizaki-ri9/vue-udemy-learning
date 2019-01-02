14f040cf64fc2f78f49c3280db93b0e1de7af725
zakizaki-ri9 <kic.yuuki@gmail.com>
2019-01-02 17:48:50 +0900

    feat: home component

--- vue-cli-webpack-simple-sample/src/App.vue
+++ vue-cli-webpack-simple-sample/src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="app">
+  <div>
+    <home-component/>
   </div>
 </template>
 
--- vue-cli-webpack-simple-sample/src/main.js
+++ vue-cli-webpack-simple-sample/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
+import Home from './Home.vue'
 import App from './App.vue'
+
+Vue.component('home-component', Home)
 
 new Vue({
   el: '#app',
