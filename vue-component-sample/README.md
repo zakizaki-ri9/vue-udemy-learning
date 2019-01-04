fea98edbe431c997d24d01798ca5b67b8a9e7f0a
zakizaki-ri9 <kic.yuuki@gmail.com>
2019-01-04 13:40:50 +0900

    feat: 子コンポーネントの動作追加

--- vue-component-sample/README.md
+++ vue-component-sample/README.md
@@ -16,3 +16,21 @@ yarn run build
 ```
 
 For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+# memo
+
+## UssrDetail.vueについて
+
+### switchName()
+
+親コンポーネントからの渡された`userName`の値、
+それを`switchName`で逆転している。
+
+親コンポーネントの`User.vue`の動作により、
+`userName`の値が変化する。
+
+1. `userName`の値変化
+2. `switchName`で逆転
+3. 表示
+
+の順で動作している。
--- vue-component-sample/src/components/UserDetail.vue
+++ vue-component-sample/src/components/UserDetail.vue
@@ -2,13 +2,21 @@
   <div class="component">
     <h3>You may view the User Details here</h3>
     <p>Many Details</p>
-    <p>User Name: {{ name }}</p>
+    <p>User Name: {{ switchName() }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['name']
+  props: ['userName'],
+  methods: {
+    /**
+     * userNameを逆転する
+     */
+    switchName() {
+      return this.userName.split("").reverse().join("");
+    }
+  },
 }
 </script>
 
