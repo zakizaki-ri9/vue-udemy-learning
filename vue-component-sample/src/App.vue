1898c968df588f74a810715f9fbbce436aaa3d00
zakizaki-ri9 <kic.yuuki@gmail.com>
2019-01-09 23:25:41 +0900

    feat: prettier/eslintno

--- vue-component-sample/src/App.vue
+++ vue-component-sample/src/App.vue
@@ -9,13 +9,13 @@
 </template>
 
 <script>
-import User from './components/User.vue';
+import User from "./components/User.vue";
 
 export default {
   components: {
     appUser: User
   }
-}
+};
 </script>
 
 <style>
